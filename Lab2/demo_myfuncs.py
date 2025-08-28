aff4bd22296a49631401c96aa2c20fee609fc665
TSSchaef <tschaeferstorm@gmail.com>
2025-08-28 12:59:11 -0500

    Lab 2

---
+++ Lab2/demo_myfuncs.py
@@ -0,0 +1,12 @@
+import myfuncs
+import numpy
+
+print("My exponential e^2.467: ", myfuncs.exponentiate(2.467))
+print("Numpy exponential e^2.467: ", numpy.exp(2.467))
+print("My exponential e^12.738: ", myfuncs.exponentiate(12.738))
+print("Numpy exponential e^12.738: ", numpy.exp(12.738))
+
+print("My log 2.467: ", myfuncs.logarithm(2.467))
+print("Numpy log 2.467: ", numpy.log(2.467))
+print("My log 12.738: ", myfuncs.logarithm(12.738))
+print("Numpy log 12.738: ", numpy.log(12.738))
---
+++ Lab2/myfuncs.py
@@ -0,0 +1,35 @@
+
+def factorial(x):
+    if x == 0 or x == 1:
+        return 1
+    s = 1
+    for k in range(2, x + 1):
+        s = s * k
+    return s
+
+
+def exponentiate(x):
+    e = 2.7182818284590451
+    x0 = int(round(x))
+    z = x - x0
+
+    # e^x  e^x0 (1 + z + (z^2/2!) + z^3/3! ...)
+
+    taylorDepth = 20
+    taylorSum = 1 + z
+    for k in range(2, taylorDepth):
+        taylorSum += (z**k / factorial(k))
+
+    return (e**x0) * (taylorSum)
+
+
+
+def logarithm(x):
+    NewtonDepth = 100
+    s = x
+
+    # Newton Method (repeated tangents)
+    for k in range(0, 20):
+        s = s - 1 + (x * exponentiate(-1 * s))
+    return s
+    
