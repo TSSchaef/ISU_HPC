c67cfbbe55ed2e233e989c957ddf8bbbd485c26a
TSSchaef <tschaeferstorm@gmail.com>
2025-10-07 14:22:50 -0500

    Switching workstation

---
+++ Lab13/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "matrix.h"
+
+
+int main(int argc, char *argv[]) {
+
+    
+
+    return 0;
+}
---
+++ Lab13/src/matrx.c
@@ -0,0 +1,63 @@
+#include "matrix.h"
+
+matrix new_matrix ( const int rows , const int cols){
+
+    return ;
+}
+
+matrix new_random_matrix ( const int rows , const int cols){
+
+}
+
+void print_matrix_full ( const matrix * mat , char* varname ){
+
+}
+
+matrix matrix_add ( const matrix * A, const matrix * B){
+
+}
+
+matrix matrix_sub ( const matrix * A, const matrix * B){
+
+}
+
+matrix matrix_mult ( const matrix * A, const matrix * B){
+
+}
+
+matrix matrix_dot_mult ( const matrix * A, const matrix * B){
+
+}
+
+vector new_vector ( const int size){
+
+}
+
+void print_vector_full ( const vector * vec , char* varname ){
+
+}
+
+vector vector_add ( const vector * x, const vector * y){
+
+}
+
+vector vector_sub ( const vector * x, const vector * y){
+
+}
+
+double vector_dot_mult ( const vector * x, const vector * y){
+
+}
+
+void print_scalar_full ( const double * z, char* varname ){
+
+}
+
+vector matrix_vector_mult ( const matrix * A, const vector * x){
+
+}
+
+vector solve ( const matrix * A, const vector * b){
+
+}
+
